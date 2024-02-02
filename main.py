438c312a5ab841aa0e2019ba1e506fc5061472d3
Kaitlyn Thompson <89215697+PenguinEmpress@users.noreply.github.com>
2024-02-01 21:58:42 -0600

    Finally figured it out

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    pass
+    if len(list1) == len(list2):
+        lm = []
+        for l in range(0, len(list1)):
+            lm.append(list1[l] * list2[l])
+        mm = sum(lm)     
+        return mm
+    else:
+        return "Error"
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    l1 = input()
+    l2 = input()
+        
+    one = []
+    two = []
+    for i in range(len(l1)):
+        one.append(int(l1[i]))
+    for i in range(len(l2)):
+        two.append(int(l2[i]))
+
+    a = sum_of_products(one, two)
+    
+    print(a)
